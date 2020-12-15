6513fc99c7d9b5603ee9ec704be825b4abb7548d
Afonso Bastos <afonsobastos@outlook.com>
2020-12-15 13:40:16 -0300

    Add Dom.

---
+++ js/domScript.js
@@ -0,0 +1,20 @@
+const tower_column = document.getElementsByClassName("tower__column");
+
+for(let i = 0; i < 3; i++){
+    let tower_rod = document.createElement('div');
+    tower_rod.setAttribute('id', 'rod' + i);
+    tower_rod.setAttribute('class', 'tower__rod');
+
+    tower_column.appendChild(tower_rod);
+}
+
+for(let i = 0; i < 4; i++){
+    let tower_disc = document.createElement('div');
+    tower_disc.setAttribute('id', 'disc' + i);
+    tower_disc.setAttribute('class', 'tower__disc');
+
+    let tower_rod = document.getElementById('rod0');
+    tower_rod.appendChild(tower_disc);
+
+}
+
--- js/script.js
+++ js/script.js
@@ -3,51 +3,28 @@
     const rod = document.querySelectorAll('.tower__rod');
     const disc = document.querySelectorAll('.tower__disc');
 
-    originRod();
+teste();
 
-function originRod() {
+function teste() {
+    for(let i = 0; i < 3; i++) {
+        rod[i].addEventListener('click',function(){
+            const firstDisc = rod[i].lastElementChild;
+            firstDisc.classList.add('tower__disc--opacity');
+            console.log(firstDisc);
+            destiny(firstDisc);
+        });
+    }
+}
+
+       
+function destiny(disc){ 
+    for(let i = 0; i < 3; i++) {
+        rod[i].addEventListener('click',function(){
+            disc.classList.remove('tower__disc--opacity');
+            rod[i].appendChild(disc);
+        });
+    } 
     
-        rod[0].addEventListener('click',function(){
-        const firstDisc = rod[0].lastElementChild;
-        firstDisc.classList.add('tower__disc--opacity');
-        console.log(firstDisc);
-        destiny(firstDisc);
-        });
-
-        rod[1].addEventListener('click',function(){
-        const firstDisc = rod[1].lastElementChild;
-        firstDisc.classList.add('tower__disc--opacity');
-        console.log(firstDisc);
-        destiny(firstDisc);
-        });
-
-        rod[2].addEventListener('click',function(){
-        const firstDisc = rod[2].lastElementChild;
-        firstDisc.classList.add('tower__disc--opacity');
-        console.log(firstDisc);
-        destiny(firstDisc);
-        });
-        
 };
 
-function destiny(disc){            
-    
-    rod[0].addEventListener('click',function(){
-        disc.classList.remove('tower__disc--opacity');
-        rod[0].appendChild(disc);
-        originRod();
-    });
 
-    rod[1].addEventListener('click',function(){
-        disc.classList.remove('tower__disc--opacity');
-        rod[1].appendChild(disc);
-        originRod();
-    });
-
-    rod[2].addEventListener('click',function(){
-        disc.classList.remove('tower__disc--opacity');
-        rod[2].appendChild(disc);
-        originRod();
-    });
-
-};
